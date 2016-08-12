8ae57ffccb724df2a20c94e4f9ef925143e07a40
Wilson Cai <wcai@berkeley.edu>
2016-08-12 12:35:01 -0700

    colname fix

--- R/table.adaptive.R
+++ R/table.adaptive.R
@@ -1,8 +1,10 @@
 #' @export table.adaptive
 table.adaptive <- function(adapt.fit) {
-	out.table <- cbind(adapt.fit[[1]], adapt.fit[[2]], adapt.fit[[3]], adapt.fit[[4]], adapt.fit[[6]], adapt.fit[[7]]*100)
+	# out.table <- cbind(adapt.fit[[1]], adapt.fit[[2]], adapt.fit[[3]], adapt.fit[[4]], adapt.fit[[6]], adapt.fit[[7]]*100)
+	out.table <- cbind(adapt.fit[[2]], adapt.fit[[3]], adapt.fit[[4]], adapt.fit[[6]], adapt.fit[[7]]*100)
 	out.table <- as.data.frame(out.table)
 	out.table[,1] <- as.integer(out.table[,1])
+	out.table <- cbind(adapt.fit[[1]], out.table)
 	names(out.table) <- c('top covariates', 'ATE estiamtes', 'raw p-values', 'adjusted p-values', 'mean CV-rank', paste('% appearance in top', length(adapt.fit[[1]])))
 
 	return(out.table)
