c1eafa1a2dfe9159e8a8d2b73a93b492db24cfa7
Wilson Cai <wcai@berkeley.edu>
2016-11-15 23:06:24 -0800

    fix table method index

--- R/table.data_adapt.R
+++ R/table.data_adapt.R
@@ -8,7 +8,7 @@
 #' @export table.data_adapt
 #'
 table.data_adapt <- function(adapt.fit, ...) {
-    
+
 	out.table <- cbind(adapt.fit$ATE.subset, adapt.fit$p.init, adapt.fit$p.final,
 	                   adapt.fit$top.mean.rank, adapt.fit$p.in.top.rank * 100)
 	out.table <- as.data.frame(out.table)
@@ -17,7 +17,7 @@ table.data_adapt <- function(adapt.fit, ...) {
 	out.table <- cbind(adapt.fit$top.col.name, out.table)
 	names(out.table) <- c('top covariates', 'ATE estiamtes', 'raw p-values',
 												'adjusted p-values', 'mean CV-rank',
-												paste('% appearance in top', length(adapt.fit[[1]])))
+												paste('% appearance in top', length(adapt.fit$top.col.name)))
 
 	return(out.table)
 }
