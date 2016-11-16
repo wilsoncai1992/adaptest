166a76bd211f15a0a787b6286f9992bb08f7c16f
Wilson Cai <wcai@berkeley.edu>
2016-11-15 22:22:14 -0800

    fix table method naming

--- R/table.data_adapt.R
+++ R/table.data_adapt.R
@@ -8,12 +8,13 @@
 #' @export table.data_adapt
 #'
 table.data_adapt <- function(adapt.fit, ...) {
-	out.table <- cbind(adapt.fit[[2]], adapt.fit[[3]], adapt.fit[[4]],
-										 adapt.fit[[6]], adapt.fit[[7]] * 100)
+    
+	out.table <- cbind(adapt.fit$ATE.subset, adapt.fit$p.init, adapt.fit$p.final,
+	                   adapt.fit$top.mean.rank, adapt.fit$p.in.top.rank * 100)
 	out.table <- as.data.frame(out.table)
 
 	# insert colnames
-	out.table <- cbind(adapt.fit[[1]], out.table)
+	out.table <- cbind(adapt.fit$top.col.name, out.table)
 	names(out.table) <- c('top covariates', 'ATE estiamtes', 'raw p-values',
 												'adjusted p-values', 'mean CV-rank',
 												paste('% appearance in top', length(adapt.fit[[1]])))
